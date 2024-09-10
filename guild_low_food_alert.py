05c9029545d43be18efb4f7e56c5aeafedd68217
Emythiel <18901520+Emythiel@users.noreply.github.com>
2024-09-10 23:46:30 +0200

    i can't spell

--- guild_low_food_alert.py
+++ guild_low_food_alert.py
@@ -39,7 +39,7 @@ def get_guild_storage():
     response.raise_for_status()
     return response.json()
 
-# Posts a message to the Discord webhook
+# Post a message to the Discord webhook
 def post_to_discord(embeds):
     payload = {
         "content": "The following food is running low in the guild storage!",
